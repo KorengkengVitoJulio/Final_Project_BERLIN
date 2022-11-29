31aae9a2688285b84ed519c76cde9617e91f3b6b
Vicky Ravensky Pati Ani <90224725+raven08@users.noreply.github.com>
2022-11-29 23:46:04 +0800

    add file

---
+++ src/components/Blog/index.js
@@ -0,0 +1,78 @@
+import React from 'react'
+
+function Blog() {
+  return (
+    <div id="blog" className="blog">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="titlepage">
+            <h2>Our Blog</h2>
+            <span>Lorem Ipsum is that it has a more-or-less normal distribution of letters,</span> 
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+          <div className="blog-box">
+            <figure><img src="images/blog-image0.jpg" alt="#" />
+              <span>10 August 2019</span>
+            </figure>
+            <div className="travel">
+              <span>Post  By :  Travel  Agency</span> 
+              <p><strong className="Comment"> 06 </strong>  Comment</p>
+              <p><strong className="like">05 </strong>Like</p>
+            </div>
+            <h3>London Amazing Tour</h3>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web</p>
+          </div>
+        </div>
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+          <div className="blog-box">
+            <figure><img src="images/blog-image.jpg" alt="#" />
+              <span>12 August 2019</span>
+            </figure>
+            <div className="travel">
+              <span>Post  By :  Travel  Agency</span> 
+              <p><strong className="Comment"> 06 </strong>  Comment</p>
+              <p><strong className="like">05 </strong>Like</p>
+            </div>
+            <h3>London Amazing Tour</h3>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web</p>
+          </div>
+        </div>
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+          <div className="blog-box">
+            <figure><img src="images/blog-image.jpg" alt="#" />
+              <span>14 August 2019</span>
+            </figure>
+            <div className="travel">
+              <span>Post  By :  Travel  Agency</span> 
+              <p><strong className="Comment"> 06 </strong>  Comment</p>
+              <p><strong className="like">05 </strong>Like</p>
+            </div>
+            <h3>London Amazing Tour</h3>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web</p>
+          </div>
+        </div>
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+          <div className="blog-box">
+            <figure><img src="images/blog-image0.jpg" alt="#" />
+              <span>15 August 2019</span>
+            </figure>
+            <div className="travel">
+              <span>Post  By :  Travel  Agency</span> 
+              <p><strong className="Comment"> 06 </strong>  Comment</p>
+              <p><strong className="like">05 </strong>Like</p>
+            </div>
+            <h3>London Amazing Tour</h3>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Blog
