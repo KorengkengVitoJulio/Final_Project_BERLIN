a6ed4f24664c9b41167070e93cf38bd06086850f
TimothyMulalinda <90163501+TimothyMulalinda@users.noreply.github.com>
2022-11-29 23:27:51 +0800

    add file

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './components/About';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Screenshot from './components/Screenshot';
+import Section from './components/Section';
+import Service from './components/Service';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Section/>
+      <About/>
+      <Blog/>
+      <Screenshot/>
+      <Footer/>
+      <Service/>
+      <Contact/>
+      
+      
+      
+      
+      
+      
+
+    </div>
+
+  );
+}
+
+export default App;
